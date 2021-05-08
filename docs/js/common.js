ecfa703bee2021c592bc2a30d84d672fe894a7c4
Yuya Makiyama <mackyym120307@gmail.com>
2021-05-08 15:51:03 +0900

    JSファイルを分割

---
+++ docs/js/common.js
@@ -0,0 +1,49 @@
+$(function () {
+  // スムーススクロール
+  function smoothScroll(_this) {
+    var href = $(_this).attr("href");
+    var target = $(href == "#" || href == "" ? 'html' : href);
+    var position = target.offset().top - 40;
+    $("html, body").animate({ scrollTop: position }, 500, "swing");
+    return false;
+  }
+  $('a[href^="#"]').not(".dropdown-toggle,[data-toggle='modal'],[data-toggle='collapse'],[data-toggle='tab']").on('click', function () {
+    smoothScroll(this);
+  });
+
+  // 下スクロールでheaderを非表示
+  var startPos = 0;
+  var scrollPos = 0;
+  if ($('.top-image').length == 0) {
+    // ".top-image"がないとき
+    $(window).on('scroll', function () {
+      scrollPos = $(this).scrollTop();
+      if (scrollPos >= startPos) {
+        $('#header').addClass('hide');
+      } else {
+        $('#header').removeClass('hide');
+      }
+      startPos = scrollPos;
+    })
+  } else {
+    // ".top-image"があるとき
+    var topImageHeight = $('.top-image').height();
+    $(window).on('scroll', function () {
+      scrollPos = $(this).scrollTop();
+      if (scrollPos >= topImageHeight) {
+        if (scrollPos >= startPos) {
+          $('#header').addClass('hide');
+        } else {
+          $('#header').removeClass('hide');
+        }
+      }
+      
+      if (scrollPos >= topImageHeight) {
+        $('#header').addClass('white-bg');
+      } else {
+        $('#header').removeClass('white-bg');
+      }
+      startPos = scrollPos;
+    })
+  }
+})
---
+++ docs/js/coworking_form.js
@@ -0,0 +1,74 @@
+$(function () {
+  $('[name="contact_type"]').change(function () {
+    var contactType = $('[name="contact_type"] option:selected').val();
+    if (contactType == "1" || contactType == "2") {
+      $("#form_preferred_visit_date").show();
+      $("#form_preferred_visit_time").show();
+      $('[name="preferred_visit_date"]').attr("required", "true");
+      $('[name="preferred_visit_time"]').attr("required", "true");
+      if (contactType == "1") {
+        if (
+          $('[name="preferred_visit_time"]').children(
+            'option[value="20:00"]'
+          ).length == 0
+        ) {
+          $('[name="preferred_visit_time"]').append(
+            '<option value="20:00">20:00~21:00</option>'
+          );
+        }
+      } else {
+        if (
+          $('[name="preferred_visit_time"]').children(
+            'option[value="20:00"]'
+          ).length == 1
+        ) {
+          $('[name="preferred_visit_time"]')
+            .children('option[value="20:00"]')
+            .remove();
+        }
+      }
+    } else {
+      $("#form_preferred_visit_date").hide();
+      $("#form_preferred_visit_time").hide();
+      $('[name="preferred_visit_date"]').removeAttr("required");
+      $('[name="preferred_visit_time"]').removeAttr("required");
+    }
+  });
+});
+
+$("#inquiryForm").submit(function (event) {
+  event.preventDefault();
+  $("#send_button").prop("disabled", true);
+  var postUrl =
+    "https://script.google.com/macros/s/AKfycbwbcIIiHad2j7kq41e0IA1PYjCD6f3wK522sgUIQYPI1qUeZi5U/exec";
+  var postData = $("#inquiryForm").serialize();
+  var posting = $.post(postUrl, postData)
+    .done(function (data) {
+      if (data.message === "reserved") {
+        $("#reservedErrorMessage").show();
+        $("#send_button").prop("disabled", false);
+      } else {
+        gtag("event", "conversion", {
+          event_category: "coworking",
+          event_label: "inquiry",
+          value: 1,
+        });
+        var callback = function () {
+          if (typeof url != "undefined") {
+            window.location = url;
+          }
+        };
+        gtag("event", "conversion", {
+          send_to: "AW-701518947/lWC1CNWfhbABEOOowc4C",
+          event_callback: callback,
+        });
+        $("#reservedErrorMessage").hide();
+        $("#inquiryForm").hide();
+        $("#thanksMessage").show();
+      }
+    })
+    .fail(function () {
+      $("#errorMessage").show();
+      $("#send_button").prop("disabled", false);
+    });
+});
